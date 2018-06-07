ba033500afd84d82520d6af0fdd96884cb074cbb
ajay.singh01 <ajay.singh01@IRIS-CSG-605>
2018-06-07 17:16:56 +0530

    Enployee View added

--- Implementation/Global.asax.cs
+++ Implementation/Global.asax.cs
@@ -20,6 +20,27 @@ namespace Implementation
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer<ApplicationDbContext>(new ApplicationContextSeedInitializer());
+        }
+    }
+
+    public class ApplicationContextSeedInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            Employee_Type type1 = new Employee_Type()
+            {
+                EmployeeType = "Peremanent Employee"
+            };
+
+            Employee_Type type2 = new Employee_Type()
+            {
+                EmployeeType = "Contract Employee"
+            };
+
+            context.EmployeeTypes.Add(type1);
+            context.EmployeeTypes.Add(type2);
+            context.SaveChanges();
         }
     }
 }
--- Implementation/Models/EmployeeViewModels.cs
+++ Implementation/Models/EmployeeViewModels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -8,20 +9,37 @@ namespace Implementation.Models
 {
     public class Employee_Type
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         public string EmployeeType { get; set; }
+
+        public virtual Employee Employee { get; set; }
     }
 
     public class Employee
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         public string JobDescription { get; set; }
+
+        [Required]
         public string ContactNumber { get; set; }
+
         public string Department { get; set; }
-        public string HourlyPay { get; set; }
-        public string Bonus { get; set; }
-        public Employee_Type Employee_Types { get; set; }
+
+        public decimal HourlyPay { get; set; }
+
+        public decimal Bonus { get; set; }
+
+        [ForeignKey("Employee_Type")]
+        public int EmployeeTypeId { get; set; }
+
+        public virtual ICollection<Employee_Type> EmployeeTypes { get; set; }
 
     }
 }
